f85dffac4c3dadae557da7f769f96480feae023b
Mehul Raj <71964538+MrM-7@users.noreply.github.com>
2021-09-09 13:08:01 +0530

    Added Initial Files.

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+var btn = document.querySelector(".switch-btn");
+var video = document.querySelector(".video-container");
+
+btn.addEventListener("click",function(){
+    if(!btn.classList.contains("slide")){
+        btn.classList.add("slide");
+        video.pause();
+    }
+    else{
+        btn.classList.remove("slide");
+        video.play();
+    }
+});
+
+const preloader = document.querySelector(".preloader")
+
+window.addEventListener("load",function(){
+    this.setTimeout(function(){
+        preloader.classList.add("hide-preloader");
+    },1000);  
+});
